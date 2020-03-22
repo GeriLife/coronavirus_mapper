fe41aa342d08e749645ccf14a9a2bb4402d75180
Brylie Christopher Oxley <brylie@amble.fi>
2020-03-22 18:48:59 +0200

    Initial Report model create/update DateTimes

--- coronavirus_mapper/reports/models.py
+++ coronavirus_mapper/reports/models.py
@@ -1,3 +1,7 @@
+from django.contrib.gis.db.models import PointField
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
