cfcc7da65e97b6dd2ea28294ee60d670bb5ade16
Brylie Christopher Oxley <brylie@amble.fi>
2020-03-22 19:22:41 +0200

    Register Report with admin

--- coronavirus_mapper/reports/admin.py
+++ coronavirus_mapper/reports/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Report
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(OSMGeoAdmin):
+    default_lon = 0
+    default_lat = 0
+    default_zoom = 1
