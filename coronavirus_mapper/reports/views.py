ee9092b0261eddc27075d77820bbff90c798191d
Brylie Christopher Oxley <brylie@amble.fi>
2020-03-22 21:31:05 +0200

    Initial Report viewset

--- coronavirus_mapper/reports/views.py
+++ coronavirus_mapper/reports/views.py
@@ -1,7 +1,19 @@
 from django.views.generic import ListView
+
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework_gis.filters import InBBoxFilter
+
 from .models import Report
+from .serializers import ReportSerializer
 
 
 class ReportListView(ListView):
     model = Report
     template_name = "reports/map.html"
+
+
+class ReportViewSet(ReadOnlyModelViewSet):
+    queryset = Report.objects.all()
+    bbox_filter_field = "location"
+    filter_backends = (InBBoxFilter, )
+    serializer_class = ReportSerializer
