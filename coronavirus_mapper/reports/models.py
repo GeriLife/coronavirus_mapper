e804587391f8798f9c84ee2fba783c47272c9577
Brylie Christopher Oxley <brylie@amble.fi>
2020-03-22 19:07:24 +0200

    Add "first symptomatic" field

--- coronavirus_mapper/reports/models.py
+++ coronavirus_mapper/reports/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.gis.db.models import PointField
 from django.db import models
 
@@ -5,3 +7,5 @@ from django.db import models
 class Report(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    first_symptomatic = models.DateField(default=datetime.now)
