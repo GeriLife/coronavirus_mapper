6fd65dab86606efdcd2c38429a7bcea3dbe2150e
Brylie Christopher Oxley <brylie@amble.fi>
2020-03-22 16:41:09 +0200

    Add database credentials

--- coronavirus_mapper/coronavirus_mapper/settings.py
+++ coronavirus_mapper/coronavirus_mapper/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'coronavirus_mapper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # TODO: use environment variables for database credentials
+        'HOST': 'localhost:5432',
+        'USER': 'postgres',
+        'PASSWORD': 'secretpassword',
+        'NAME': 'coronavirus_mapper'
+
     }
 }
 
